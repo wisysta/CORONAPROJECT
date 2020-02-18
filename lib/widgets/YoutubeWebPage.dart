12ebbd9340fb005cbdf66737b2aafc2e2f7b84f6
wisysta <choi199042@gmail.com>
2020-02-18 19:22:00 +0900

    아이콘 추가

--- lib/widgets/YoutubeWebPage.dart
+++ lib/widgets/YoutubeWebPage.dart
@@ -1,12 +1,19 @@
+import 'package:corona_project/widgets/LoadingWrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+class YoutubeWebPage extends StatefulWidget {
+  String pageLink;
 
+  YoutubeWebPage({Key key, this.pageLink}) : super(key: key);
 
-class YoutubeWebPage extends StatelessWidget {
+  @override
+  _YoutubeWebPageState createState() => _YoutubeWebPageState();
+}
+
+class _YoutubeWebPageState extends State<YoutubeWebPage> {
   var pageLink;
-
-  YoutubeWebPage({Key key, @required this.pageLink}) : super(key: key);
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +21,38 @@ class YoutubeWebPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
             leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios,
-              size: 22,color: Colors.pink,),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: WebView(
-                  initialUrl: pageLink,
-                  javascriptMode: JavascriptMode.unrestricted),
-            ),
-          ],
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 22,
+            color: Colors.pink,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        )),
+        body: LoadingWrapper(
+          isLoading: _isLoading,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: WebView(
+                    onPageStarted: (e) {
+                      setState(() {
+                        _isLoading = true;
+                      });
+                    },
+                    onPageFinished: (e) {
+                      setState(() {
+                        _isLoading = false;
+                      });
+                    },
+                    initialUrl: widget.pageLink,
+                    javascriptMode: JavascriptMode.unrestricted),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
